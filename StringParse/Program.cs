316c54965503cbf5c75e53f694bb3626ec6eb72d
AlexProstolucky <alexprostolucky@gmail.com>
2023-04-07 18:43:06 +0300

    Добавьте файлы проекта.

---
+++ StringParse/Program.cs
@@ -0,0 +1,31 @@
+﻿internal class Program
+{
+    static void Main(string[] args)
+    {
+        Console.InputEncoding = System.Text.Encoding.Unicode;
+        Console.OutputEncoding = System.Text.Encoding.Unicode;
+        Console.WriteLine("Напишіть свій текст:");
+        string str = Console.ReadLine();
+        int flag = 0;
+        int start = 0, end = 0;
+        for (int i = 0; i < str.Length; i++)
+        {
+            if (str[i] == ';')
+            {
+                if (flag == 3)
+                {
+                    end = i;
+                    Console.WriteLine("\nID: ");
+                    Console.Write(str.Substring(start, end - start));
+                    break;
+                }
+                flag++;
+                if (flag == 3)
+                {
+                    start = i + 1;
+                }
+            }
+        }
+        Console.ReadKey();
+    }
+}
